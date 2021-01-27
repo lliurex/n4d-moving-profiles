fa1779d091c936131a0bd683530bc40d272893fb
Lt-Henry <quiqueiii@gmail.com>
2021-01-27 12:19:45 +0100

    * upgraded plugin to new n4d framework * moved to a flat file tree

--- setup.py
+++ setup.py
@@ -4,17 +4,14 @@ from distutils.extension import Extension
 import os
 import subprocess
 
-
-
 setup(  name             = "n4d-moving-profiles",
-        version          = "2.0",
+        version          = "2.5",
         author           = "Enrique Medina Gremaldos",
         author_email     = "quiqueiii@gmail.com",
-        url              = "http://lliurex.net/home/",
-        data_files  =   [("/usr/share/n4d/python-plugins/",["install/usr/share/n4d/python-plugins/MovingProfiles.py"]),
-                        ("/usr/share/n4d-moving-profiles/",["install/usr/share/n4d-moving-profiles/setup.conf"]),
-                        ("/etc/n4d/conf.d/",["install/etc/n4d/conf.d/MovingProfiles"])
+        url              = "https://github.com/lliurex/n4d-moving-profiles",
+        data_files  =   [("/usr/share/n4d/python-plugins/",["MovingProfiles.py"]),
+                        ("/etc/n4d-moving-profiles/",["setup.conf"]),
+                        ("/etc/n4d/conf.d/",["MovingProfiles.json"])
                         ]
         
      )
-
